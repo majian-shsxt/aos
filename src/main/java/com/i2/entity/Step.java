2956e2aea9704fbff749050517f3f73abeca5f75
majian-shsxt <1554270226@qq.com>
2018-05-25 16:02:19 +0800

    2018.5.25

--- src/main/java/com/i2/entity/Step.java
+++ src/main/java/com/i2/entity/Step.java
@@ -1,4 +1,94 @@
 package com.i2.entity;
 
 public class Step {
+    public  String step_id;
+    public  String step_name;
+    public  String step_desc;
+    public  String step_type;
+    public  String step_module;
+    public  String step_ref;
+    public  String step_matches;
+    public  String step_args;
+    public  String lst_update_date;
+    public  String lst_update_user;
+
+    public String getStep_id() {
+        return step_id;
+    }
+
+    public void setStep_id(String step_id) {
+        this.step_id = step_id;
+    }
+
+    public String getStep_name() {
+        return step_name;
+    }
+
+    public void setStep_name(String step_name) {
+        this.step_name = step_name;
+    }
+
+    public String getStep_desc() {
+        return step_desc;
+    }
+
+    public void setStep_desc(String step_desc) {
+        this.step_desc = step_desc;
+    }
+
+    public String getStep_type() {
+        return step_type;
+    }
+
+    public void setStep_type(String step_type) {
+        this.step_type = step_type;
+    }
+
+    public String getStep_module() {
+        return step_module;
+    }
+
+    public void setStep_module(String step_module) {
+        this.step_module = step_module;
+    }
+
+    public String getStep_ref() {
+        return step_ref;
+    }
+
+    public void setStep_ref(String step_ref) {
+        this.step_ref = step_ref;
+    }
+
+    public String getStep_matches() {
+        return step_matches;
+    }
+
+    public void setStep_matches(String step_matches) {
+        this.step_matches = step_matches;
+    }
+
+    public String getStep_args() {
+        return step_args;
+    }
+
+    public void setStep_args(String step_args) {
+        this.step_args = step_args;
+    }
+
+    public String getLst_update_date() {
+        return lst_update_date;
+    }
+
+    public void setLst_update_date(String lst_update_date) {
+        this.lst_update_date = lst_update_date;
+    }
+
+    public String getLst_update_user() {
+        return lst_update_user;
+    }
+
+    public void setLst_update_user(String lst_update_user) {
+        this.lst_update_user = lst_update_user;
+    }
 }
---
+++ src/main/java/com/i2/entity/Template.java
@@ -0,0 +1,58 @@
+package com.i2.entity;
+
+public class Template {
+    public Integer template_id;
+    public String template_name;
+    public String template_desc;
+    public String template_flowcontent;
+    public String lst_update_date;
+    public String lst_update_user;
+
+    public Integer getTemplate_id() {
+        return template_id;
+    }
+
+    public void setTemplate_id(Integer template_id) {
+        this.template_id = template_id;
+    }
+
+    public String getTemplate_name() {
+        return template_name;
+    }
+
+    public void setTemplate_name(String template_name) {
+        this.template_name = template_name;
+    }
+
+    public String getTemplate_desc() {
+        return template_desc;
+    }
+
+    public void setTemplate_desc(String template_desc) {
+        this.template_desc = template_desc;
+    }
+
+    public String getTemplate_flowcontent() {
+        return template_flowcontent;
+    }
+
+    public void setTemplate_flowcontent(String template_flowcontent) {
+        this.template_flowcontent = template_flowcontent;
+    }
+
+    public String getLst_update_date() {
+        return lst_update_date;
+    }
+
+    public void setLst_update_date(String lst_update_date) {
+        this.lst_update_date = lst_update_date;
+    }
+
+    public String getLst_update_user() {
+        return lst_update_user;
+    }
+
+    public void setLst_update_user(String lst_update_user) {
+        this.lst_update_user = lst_update_user;
+    }
+}
